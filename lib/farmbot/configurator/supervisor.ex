af00831b5906bb538ecf2f88aa0073c3f4eac02f
connor rigby <connor@farmbot.io>
2018-06-14 09:13:23 -0700

    Add missing file.

---
+++ lib/farmbot/configurator/supervisor.ex
@@ -0,0 +1,17 @@
+defmodule Farmbot.Configurator.Supervisor do
+  use Supervisor
+  @configurator_impl Application.get_env(:farmbot, :behaviour)[:configurator]
+  @configurator_impl || Mix.raise("Please configure a configurator implementation.")
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], [name: __MODULE__])
+  end
+
+  def init([]) do
+    children = [
+      {@configurator_impl, []},
+      {Farmbot.Configurator, []}
+    ]
+    Supervisor.init(children, strategy: :one_for_all)
+  end
+end
